889a36f0de542beda43d2b47cf0bcb4360087cdc
Rafael-cmd-html <armandocastillejos086@gmail.com>
2023-12-05 07:16:09 -0700

    BACKEND DEVELOVPMENT BEGINS

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:machapstore1/screens/login.dart';
 import 'package:machapstore1/screens/signUp.dart';
