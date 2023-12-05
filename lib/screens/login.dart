ccedbd558388e02cf0554ed0655cd7549b3c380e
Rafael-cmd-html <armandocastillejos086@gmail.com>
2023-12-05 01:40:36 -0700

    Testing colors in design

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -29,106 +29,116 @@ class _login_state extends State<login> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Form(
-            key: _form_key,
-            child: Container(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  height: 500,
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Stack(
-                        alignment: Alignment.center,
-                        children: [
-                          // Coloca la imagen redonda aquí
-                          CircleAvatar(
-                              radius: 50,
-                              backgroundImage: AssetImage('images/machape.png'))
-                        ],
-                      ),
-                      Text(
-                        "Login",
-                        style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold),
-                      ),
-                      TextFormField(
-                        validator: (value) {
-                          if (value == null || value == "") {
-                            return "Please fill email";
-                          } else if (!reg_exp_email.hasMatch(value)) {
-                            return "Email is invalid";
-                          }
-                          return null;
-                        },
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: "Email",
-                            hintStyle: TextStyle(color: Colors.black)),
-                      ),
-                      TextFormField(
-                        obscureText: obscure_text,
-                        validator: (value) {
-                          if (value == null) {
-                            return "Please fill password";
-                          } else if (value.length < 8) {
-                            return "Password is too short, try with 8 chars";
-                          } else if (!reg_exp_password.hasMatch(value)) {
-                            return "Password must contain at least one uppercase, one lower case, at least one digit and one special char ";
-                          }
-                        },
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: "Password",
-                            suffixIcon: GestureDetector(
-                              onTap: () {
-                                FocusScope.of(context).unfocus();
-                                setState(() {
-                                  obscure_text = !obscure_text;
-                                });
-                              },
-                              child: Icon(
-                                  obscure_text == true
-                                      ? Icons.visibility
-                                      : Icons.visibility_off,
-                                  color: Colors.black),
-                            ),
-                            hintStyle: TextStyle(color: Colors.black)),
-                      ),
-                      my_button(
-                        name: "Login",
-                        onPressed: () {
-                          validation();
-                        },
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+        body: Container(
+            color: Color(0xFF595959), // Color gris #595959
+            child: Form(
+                key: _form_key,
+                child: Container(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 500,
+                      margin: EdgeInsets.symmetric(horizontal: 10),
+                      width: double.infinity,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
-                          Text("I don't have an account"),
-                          SizedBox(
-                            width: 10,
+                          Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              // Coloca la imagen redonda aquí
+                              CircleAvatar(
+                                  radius: 50,
+                                  backgroundImage:
+                                      AssetImage('images/machape.png'))
+                            ],
                           ),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.of(context).pushReplacement(
-                                  MaterialPageRoute(
-                                      builder: (ctx) => sign_up()));
+                          Text(
+                            "Login",
+                            style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          TextFormField(
+                            style: TextStyle(color: Colors.white),
+                            validator: (value) {
+                              if (value == null || value == "") {
+                                return "Please fill email";
+                              } else if (!reg_exp_email.hasMatch(value)) {
+                                return "Email is invalid";
+                              }
+                              return null;
                             },
-                            child: Text(
-                              "SignUp",
-                              style: TextStyle(color: Colors.cyan),
-                            ),
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "Email",
+                                hintStyle: TextStyle(color: Colors.white)),
+                          ),
+                          TextFormField(
+                            obscureText: obscure_text,
+                            style: TextStyle(color: Colors.white),
+                            validator: (value) {
+                              if (value == null) {
+                                return "Please fill password";
+                              } else if (value.length < 8) {
+                                return "Password is too short, try with 8 chars";
+                              } else if (!reg_exp_password.hasMatch(value)) {
+                                return "Password must contain at least one uppercase, one lower case, at least one digit and one special char ";
+                              }
+                            },
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "Password",
+                                suffixIcon: GestureDetector(
+                                  onTap: () {
+                                    FocusScope.of(context).unfocus();
+                                    setState(() {
+                                      obscure_text = !obscure_text;
+                                    });
+                                  },
+                                  child: Icon(
+                                      obscure_text == true
+                                          ? Icons.visibility
+                                          : Icons.visibility_off,
+                                      color: Colors.white),
+                                ),
+                                hintStyle: TextStyle(color: Colors.white)),
+                          ),
+                          my_button(
+                            name: "Login",
+                            onPressed: () {
+                              validation();
+                            },
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "I don't have an account",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.of(context).pushReplacement(
+                                      MaterialPageRoute(
+                                          builder: (ctx) => sign_up()));
+                                },
+                                child: Text(
+                                  "SignUp",
+                                  style: TextStyle(color: Colors.cyan),
+                                ),
+                              )
+                            ],
                           )
                         ],
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ))));
+                      ),
+                    )
+                  ],
+                )))));
   }
 }
